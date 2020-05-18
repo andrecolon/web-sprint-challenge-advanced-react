d873d346c86c9e9debac34aee387df86a01b63c0
Andre Colon <andrecolon@gmail.com>
2020-05-17 20:59:05 -0700

    Uploaded branch - from local dev

---
+++ client/src/components/PlantList.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class PlantList extends Component {
+  // add state with a property called "plants" - initialize as an empty array
+  constructor() {
+    super();
+    this.state = {
+      plants: []
+    };
+  }
+  // when the component mounts:
+  //   - fetch data from the server endpoint - http://localhost:3333/plants
+  //   - set the returned plants array to this.state.plants
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:3333/plants")
+      .then(res => {
+        console.log(res.data.plantsData)//why can't I see this?
+        this.setState({ plants: res.data.plantsData })
+      })
+  }
+  /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
+  render() {
+    return (
+      <main className="plant-list">
+        {this.state?.plants?.map((plant) => (
+          <div className="plant-card" key={plant.id}>
+            <img className="plant-image" src={plant.img} alt={plant.name} />
+            <div className="plant-details">
+              <h2 className="plant-name">{plant.name}</h2>
+              <p className="plant-scientific-name">{plant.scientificName}</p>
+              <p>{plant.description}</p>
+              <div className="plant-bottom-row">
+                <p>${plant.price}</p>
+                <p><span role="img" aria-label="Sunlight">☀️</span> {plant.light}</p>
+                <p><span role="img" aria-label="Water">💦</span> {plant.watering}x/month</p>
+              </div>
+              <button
+                className="plant-button"
+                onClick={() => this.props.addToCart(plant)}
+              >
+                Add to cart
+              </button>
+            </div>
+          </div>
+        ))}
+      </main>
+    );
+  }
+}
---
+++ client/src/hooks/useForm.js
@@ -0,0 +1,38 @@
+import { useLocalStorage } from "./useLocalStorage";
+
+const useForm = (initialState, key, submitLogic, notStickyArray) => {
+  // const [username, setUsername] = useState("");
+  // const [email, setEmail] = useState("");
+  const [values, setValues] = useLocalStorage(
+    initialState,
+    key,
+    notStickyArray
+  );
+
+  const handleChanges = e => {
+    // setUsername(e.target.value);
+    // access the field name with e.target.name
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  // const handleEmailChanges = e => {
+  //   setEmail(e.target.value);
+  // };
+
+  const clearForm = () => {
+    setValues(initialState);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    submitLogic();
+    // send to api server to update
+  };
+
+  return [values, handleChanges, clearForm, handleSubmit];
+};
+
+export  default useForm
